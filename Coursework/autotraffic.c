e124e0b6433fb2b2b5a5c268987168f4abaeb1e4
Яков Сендов <sendov.yad@edu.spbstu.ru>
2022-06-18 01:20:03 +0300

    NEW Update 0.2

--- Coursework/autotraffic.c
+++ Coursework/autotraffic.c
@@ -11,6 +11,8 @@
 #define WINDOW_HEIGHT 800
 #define BUTTON_WIDTH 350
 #define BUTTON_HEIGHT 50
+#define MINI_BUTTON_WIDTH 70
+#define MINI_BUTTON_HEIGHT 20
 
 // COLORS
 #define COLOR_MENU_RED 0.102
@@ -20,12 +22,22 @@
 GLint Width = 800, Height = 800;
 bool menu_activity = false;
 bool menu_button_3 = false;
+bool menu_button_2 = false;
+bool menu_button_1 = false;
+bool menu_changes_2 = false;
 
 struct menu_button
 {
     int x_coords[4];
     int y_coords[4];
 };
+
+struct settings
+{
+    int interval;
+    int count_lanes;
+    bool autosave;
+} settings;
 
 void drawstring(float x, float y, char* string)
 {
@@ -90,6 +102,87 @@ void text_buttons(struct menu_button* button, int flag)
     }
 }
 
+void draw_mini_buttons(int w, int h, int flag)
+{
+    if (flag == 0) glColor3ub(138, 43, 226);
+    else if (flag == 1) glColor3ub(0, 153, 0);
+    glBegin(GL_QUADS);
+    glVertex2i(Width / 2 + w, Height - h);
+    glVertex2i(Width / 2 + w, Height - h + MINI_BUTTON_HEIGHT);
+    glVertex2i(Width / 2 + MINI_BUTTON_WIDTH + w, Height - h + MINI_BUTTON_HEIGHT);
+    glVertex2i(Width / 2 + MINI_BUTTON_WIDTH + w, Height - h);
+    glEnd();
+    if (h == 365)
+    {
+        glColor3ub(255, 255, 255);
+        if (w == 0) drawstring(w + (Width / 2) + (MINI_BUTTON_WIDTH / 2) - 5, Height - h + 5, "3");
+        else if (w == 90) drawstring(w + (Width / 2) + (MINI_BUTTON_WIDTH / 2) - 5, Height - h + 5, "5");
+        else if (w == 180) drawstring(w + (Width / 2) + (MINI_BUTTON_WIDTH / 2) - 5, Height - h + 5, "7");
+    }
+    else if (h == 395)
+    {
+        glColor3ub(255, 255, 255);
+        if (w == 0) drawstring(w + (Width / 2) + (MINI_BUTTON_WIDTH / 2) - 5, Height - h + 5, "2");
+        else if (w == 90) drawstring(w + (Width / 2) + (MINI_BUTTON_WIDTH / 2) - 5, Height - h + 5, "3");
+    }
+    else if (h == 425)
+    {
+        glColor3ub(255, 255, 255);
+        if (w == 0) drawstring(w + (Width / 2) + (MINI_BUTTON_WIDTH / 2) - 15, Height - h + 5, "yes");
+        else if (w == 90) drawstring(w + (Width / 2) + (MINI_BUTTON_WIDTH / 2) - 10, Height - h + 5, "no");
+    }
+}
+
+void interval_buttons()
+{
+    if (settings.interval == 3)
+    {
+        draw_mini_buttons(0, 365, 1);
+        draw_mini_buttons(90, 365, 0);
+        draw_mini_buttons(180, 365, 0);
+    }
+    else if (settings.interval == 5)
+    {
+        draw_mini_buttons(0, 365, 0);
+        draw_mini_buttons(90, 365, 1);
+        draw_mini_buttons(180, 365, 0);
+    }
+    else if (settings.interval == 7)
+    {
+        draw_mini_buttons(0, 365, 0);
+        draw_mini_buttons(90, 365, 0);
+        draw_mini_buttons(180, 365, 1);
+    }
+}
+
+void count_buttons()
+{
+    if (settings.count_lanes == 2)
+    {
+        draw_mini_buttons(0, 395, 1);
+        draw_mini_buttons(90, 395, 0);
+    }
+    else if (settings.count_lanes == 3)
+    {
+        draw_mini_buttons(0, 395, 0);
+        draw_mini_buttons(90, 395, 1);
+    }
+}
+
+void save_buttons()
+{
+    if (settings.autosave == true)
+    {
+        draw_mini_buttons(0, 425, 1);
+        draw_mini_buttons(90, 425, 0);
+    }
+    else if (settings.autosave == false)
+    {
+        draw_mini_buttons(0, 425, 0);
+        draw_mini_buttons(90, 425, 1);
+    }
+}
+
 void processing_buttons(int button)
 {
     int delta = BUTTON_HEIGHT;
@@ -129,11 +222,62 @@ void processing_buttons(int button)
     }
     else if (button == 2)
     {
+        menu_button_2 = true;
+        glClearColor(COLOR_MENU_RED, COLOR_MENU_GREEN, COLOR_MENU_BLUE, 0);
+        glClear(GL_COLOR_BUFFER_BIT);
+        glColor3ub(255, 255, 255);
+        //coord_lines();
+        drawstring(Width / 2 - 115, Height - 200, "CAR TRAFFIC SIMULATOR");
+        drawstring(Width / 2 - 37, Height - 300, "Settings:");
+        drawstring(Width / 10 - 34, Height - 360, "Spawn interval of machines (seconds):");
+        drawstring(Width / 4 - 6, Height - 390, "Number of car lanes:");
+        drawstring(Width / 3 + 14, Height - 420, "Autosave:");
+        
+        interval_buttons();
+        count_buttons();
+        save_buttons();
+        
+        glColor3ub(138, 43, 226);
+        glBegin(GL_QUADS);
+        glVertex2i(left, bottom + (-7) * delta / 2);
+        glVertex2i(left, top + (-7) * delta / 2);
+        glVertex2i(right, top + (-7) * delta / 2);
+        glVertex2i(right, bottom + (-7) * delta / 2);
+        glEnd();
 
+        glColor3ub(255, 255, 255);
+        drawstring(Width / 2 - 20, Height - 580, "Back");
+        menu_activity = false;
+        glutSwapBuffers();
     }
     else if (button == 1)
     {
+        menu_button_1 = true;
+        glClearColor(COLOR_MENU_RED, COLOR_MENU_GREEN, COLOR_MENU_BLUE, 0);
+        glClear(GL_COLOR_BUFFER_BIT);
+        glColor3ub(255, 255, 255);
+        //coord_lines();
+        drawstring(Width / 2 - 70, Height - 200, "Automobile traffic");
+        glColor3ub(138, 43, 226);
+        glBegin(GL_QUADS);
+        glVertex2i(left, bottom + (-7) * delta / 2);
+        glVertex2i(left, top + (-7) * delta / 2);
+        glVertex2i(right - 290, top + (-7) * delta / 2);
+        glVertex2i(right - 290, bottom + (-7) * delta / 2);
+        glEnd();
 
+        glBegin(GL_QUADS);
+        glVertex2i(left + 80, bottom + (-7) * delta / 2);
+        glVertex2i(left + 80, top + (-7) * delta / 2);
+        glVertex2i(right, top + (-7) * delta / 2);
+        glVertex2i(right, bottom + (-7) * delta / 2);
+        glEnd();
+
+        glColor3ub(255, 255, 255);
+        drawstring(Width / 2 - 165, Height - 580, "Back");
+        drawstring(Width / 2, Height - 580, "Continue");
+        menu_activity = false;
+        glutSwapBuffers();
     }
 }
 
@@ -152,6 +296,7 @@ void menu()
     text_buttons(&third_button, 3);
     text_buttons(&fourth_button, 4);
     menu_activity = true;
+    menu_changes_2 = false;
 }
 
 void display()
@@ -191,17 +336,70 @@ void mouse_pressed(int button, int state, int x, int y)
     {
     case GLUT_LEFT_BUTTON: 
     {
-        printf("%d | %d\n", x, y);
+        printf("%d | %d\n", x, y); 
         if (state == GLUT_DOWN && menu_activity == true)
         {
-            if (x <= 575 && x >= 275 && y >= 575 && y <= 625) exit(0);
-            else if (x <= 575 && x >= 275 && y >= 500 && y <= 550) processing_buttons(3);
+            if (x <= 575 && x >= 225 && y >= 575 && y <= 625) exit(0);
+            else if (x <= 575 && x >= 225 && y >= 500 && y <= 550) processing_buttons(3);
+            else if (x <= 575 && x >= 225 && y >= 425 && y <= 475) processing_buttons(2);
+            else if (x <= 575 && x >= 225 && y >= 350 && y <= 400) processing_buttons(1);
         }
         else if (state == GLUT_DOWN && menu_button_3 == true)
         {
-            if (x <= 575 && x >= 275 && y >= 550 && y <= 600)
+            if (x <= 575 && x >= 225 && y >= 550 && y <= 600)
             {
-                menu_button_3 == false;
+                menu_button_3 = false;
+                glutPostRedisplay();
+            }
+        }
+        else if (state == GLUT_DOWN && menu_button_2 == true)
+        {
+            if (x <= 575 && x >= 225 && y >= 550 && y <= 600)
+            {
+                menu_button_2 = false;
+                glutPostRedisplay();
+            }
+            else if (x <= 470 && x >= 400 && y >= 345 && y <= 365)
+            {
+                settings.interval = 3;
+                glutPostRedisplay();
+            }
+            else if (x <= 560 && x >= 490 && y >= 345 && y <= 365)
+            {
+                settings.interval = 5;
+                glutPostRedisplay();
+            }
+            else if (x <= 640 && x >= 580 && y >= 345 && y <= 365)
+            {
+                settings.interval = 7;
+                glutPostRedisplay();
+            }
+            else if (x <= 470 && x >= 400 && y >= 375 && y <= 395)
+            {
+                settings.count_lanes = 2;
+                glutPostRedisplay();
+            }
+            else if (x <= 560 && x >= 490 && y >= 375 && y <= 395)
+            {
+                settings.count_lanes = 3;
+                glutPostRedisplay();
+            }
+            else if (x <= 470 && x >= 400 && y >= 405 && y <= 425)
+            {
+                settings.autosave = true;
+                glutPostRedisplay();
+            }
+            else if (x <= 560 && x >= 490 && y >= 405 && y <= 425)
+            {
+                settings.autosave = false;
+                glutPostRedisplay();
+            }
+        }
+        else if (state == GLUT_DOWN && menu_button_1 == true)
+        {
+            if (x <= 285 && x >= 225 && y >= 550 && y <= 600)
+            {
+                menu_button_1 = false;
                 glutPostRedisplay();
             }
         }
@@ -218,12 +416,20 @@ void keyboard(unsigned char key, int x, int y)
         exit(0);
 }
 
+void struct_init()
+{
+    settings.interval = 5;
+    settings.autosave = false;
+    settings.count_lanes = 3;
+}
+
 int main(int argc, char* argv[])
 {
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE);
     glutInitWindowSize(WINDOW_WIDTH, WINDOW_HEIGHT);
     glutCreateWindow(WINDOW_NAME);
+    struct_init();
 
     glutDisplayFunc(display);
     glutReshapeFunc(reshape);
